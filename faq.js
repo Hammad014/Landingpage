5c783561f3739fb7684ed8e2ba604fcd4e81a664
Hammad014 <hammadworld011@gmail.com>
2023-10-04 00:06:09 +0500

    Updated project with new changes

--- faq.js
+++ faq.js
@@ -5,12 +5,12 @@ function togglefunction(faqNumber) {
     for (let i = 0; i < faqDetails.length; i++) {
         if (i + 1 === faqNumber) {
             if (faqDetails[i].style.display === 'block') {
-                faqDetails[i].style.display = 'none'; // Close the FAQ details
+                faqDetails[i].style.display = 'none'; 
             } else {
-                faqDetails[i].style.display = 'block'; // Open the FAQ details
+                faqDetails[i].style.display = 'block'; 
             }
         } else {
-            faqDetails[i].style.display = 'none'; // Hide other FAQ details
+            faqDetails[i].style.display = 'none'; 
         }
     }
 }
